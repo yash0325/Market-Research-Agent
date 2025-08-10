aed3f634d0aea4cddd4a75425b320a26568fec0d
yash0325 <112287154+yash0325@users.noreply.github.com>
2025-08-10 12:57:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 # LLM / LangChain
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
